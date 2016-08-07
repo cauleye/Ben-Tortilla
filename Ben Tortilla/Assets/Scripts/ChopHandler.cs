6bca84284a32e7279ec9824750e82d750c08ca3e
elijahcauley <elijah.cauley@lawrence.edu>
2016-08-07 18:18:47 -0400

    Added dragging functionality and basic chopping functionality. Included some placeholder tomato and knife art.

---
+++ Ben Tortilla/Assets/Scripts/ChopHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChopHandler : MonoBehaviour {
+
+	public bool touchingKnife;
+
+	public SpriteRenderer spriteRenderer;
+	public Sprite wholeFood;
+	public Sprite halfFood;
+	public Sprite manyFood;
+
+	public AudioSource audioSource;
+	public AudioClip chopSound; //This is the sound for chopping
+	public AudioClip noChop; //This is for when the player has chopped all the way through
+
+	// Use this for initialization
+	void Start () {
+		touchingKnife = false; 
+		spriteRenderer.sprite = wholeFood;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("space")){
+			if (touchingKnife) {
+				if (spriteRenderer.sprite == wholeFood) {
+					audioSource.PlayOneShot (chopSound);
+					spriteRenderer.sprite = halfFood;
+				} else if (spriteRenderer.sprite == halfFood) {
+					spriteRenderer.sprite = manyFood;
+					audioSource.PlayOneShot (chopSound);
+				} else { 
+					audioSource.PlayOneShot (noChop);
+				}
+			}
+		}
+	}
+
+	//These check to see if the object is touching the knife or not
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.transform.name == "Knife") {
+			touchingKnife = true; 
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.transform.name == "Knife") {
+			touchingKnife = false; 
+		}
+	}
+}
