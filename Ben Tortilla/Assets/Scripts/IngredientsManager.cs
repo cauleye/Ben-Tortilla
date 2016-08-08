1940bc7a78bd927eeb61820c6a56a48de961e46a
elijahcauley <elijah.cauley@lawrence.edu>
2016-08-08 09:42:00 -0400

    Added IngredientsManager and InstantiateFood scripts, making the buttons that spawn food in scene 03 work.

---
+++ Ben Tortilla/Assets/Scripts/IngredientsManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientsManager : MonoBehaviour {
+
+	public int tomatoCount;
+	public int onionCount;
+	public int pepperCount;
+	public int chorizoCount;
+	public int cheeseCount;
+
+	// Use this for initialization
+	void Start () {
+		tomatoCount = 0;
+		onionCount = 0;
+		pepperCount = 0;
+		chorizoCount = 0;
+		cheeseCount = 0;
+	}
+
+	//This prevents the script from being destroyed changing from scene 03 to 04
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+		Debug.Log("Don't destroy on load: " + name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void AddTomato(){
+		tomatoCount++;
+	}
+
+	public void AddOnion(){
+		onionCount++;
+	}
+
+	public void AddPepper(){
+		pepperCount++;
+	}
+
+	public void AddChorizo(){
+		chorizoCount++;
+	}
+
+	public void AddCheese(){
+		cheeseCount++;
+	}
+}
---
+++ Ben Tortilla/Assets/Scripts/InstantiateFood.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateFood : MonoBehaviour {
+
+	public GameObject food;
+
+	public Transform foodSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreateFood(){
+		//The following sets up a small amount of randomness in how the food spawns in 
+		Vector3 foodSpawnRandom = new Vector3 (foodSpawn.position.x + Random.Range (-2, 2), foodSpawn.position.y + Random.Range (-2, 2), foodSpawn.position.z);
+		Instantiate (food, foodSpawnRandom, Quaternion.identity);
+	}
+}
