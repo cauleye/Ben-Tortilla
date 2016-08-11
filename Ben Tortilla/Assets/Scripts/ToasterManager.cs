b024ed70be5154d7b9d40b34a06ce9bc26e2b3fc
elijahcauley <elijah.cauley@lawrence.edu>
2016-08-11 15:17:33 -0400

    Set up 05, toaster now toasts. Used some temporary images of the toaster to do the animation.

---
+++ Ben Tortilla/Assets/Scripts/ToasterManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToasterManager : MonoBehaviour {
+
+	public Animator animator;
+
+	public GameObject toastButton;
+	public GameObject continueButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Toast(){
+		toastButton.SetActive (false);
+		animator.Play ("Toasting");
+		continueButton.SetActive (true);
+	}
+}
