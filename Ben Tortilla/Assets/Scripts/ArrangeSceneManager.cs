e0cbfb3cd7a6152c77458d061a72bb9c90d1907c
elijahcauley <elijah.cauley@lawrence.edu>
2016-08-11 12:14:31 -0400

    Chorizo and Tomato prefabs are now persistent between scenes 03 and 04. Will destroy themselves in scene 05. Removed the necessity of using a SceneManager to track the number of objects and them spawn them back in.

--- Ben Tortilla/Assets/Scripts/ArrangeSceneManager.cs
+++ Ben Tortilla/Assets/Scripts/ArrangeSceneManager.cs
@@ -3,22 +3,50 @@ using System.Collections;
 
 public class ArrangeSceneManager : MonoBehaviour {
 
-
 	public GameObject managerObject;
 	public IngredientsManager ingredientsManager;
 
+	public GameObject createTomato;
+	public InstantiateFood tomatoInstantiator;
+	public GameObject createChorizo;
+	public InstantiateFood chorizoInstantiator;
 
 	// Use this for initialization
 	void Start () {
 		managerObject = GameObject.Find("Ingredient Manager");
 		ingredientsManager = managerObject.GetComponent<IngredientsManager> ();
 
+		createTomato = GameObject.Find ("CreateTomato");
+		tomatoInstantiator = createTomato.GetComponent<InstantiateFood> ();
+		createChorizo = GameObject.Find ("CreateChorizo");
+		chorizoInstantiator = createChorizo.GetComponent<InstantiateFood> ();
 
 
+
+		SpawnTomatos ();
+		SpawnChorizo ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	//These scripts check the ingredients manager to see how many of each food the player prepared in the last scene
+	//in order to spawn in the same number of ingredients in this scene. 
+	void SpawnTomatos(){
+		int tomatoCount = ingredientsManager.tomatoCount;
+		for (int i = 0; i < tomatoCount; i++) {
+			tomatoInstantiator.CreateFood ();
+		}
+	}
+
+	void SpawnChorizo(){
+		int chorizoCount = ingredientsManager.chorizoCount;
+		for (int i = 0; i < chorizoCount; i++) {
+			chorizoInstantiator.CreateFood ();
+		}
+	}
+
+
 }
---
+++ Ben Tortilla/Assets/Scripts/FoodPersistenceManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class FoodPersistenceManager : MonoBehaviour {
+
+	//This is to make the food object pass from 03 where it was created to 04
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destroy on load: " + name);
+	}
+
+	void Update(){
+		Scene scene = SceneManager.GetActiveScene ();
+		if (scene.name == "04 Arrange") {
+			Destroy (gameObject);
+		}
+	}
+}
